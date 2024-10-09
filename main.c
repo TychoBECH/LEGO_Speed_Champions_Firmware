d250efd77d3ec25e24b82b795ccdce8384264112
TychoBECH <s1u.tycho.kropf@gmail.com>
2024-10-09 14:20:59 +0200

    Removed unused comments and NOP() for debugging

--- main.c
+++ main.c
@@ -51,21 +51,6 @@ int main(void)
 {
     SYSTEM_Initialize();
 
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts 
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts 
-    // Use the following macros to: 
-
-    // Enable the Global Interrupts 
-    //INTERRUPT_GlobalInterruptEnable(); 
-
-    // Disable the Global Interrupts 
-    //INTERRUPT_GlobalInterruptDisable(); 
-
-    // Enable the Peripheral Interrupts 
-    //INTERRUPT_PeripheralInterruptEnable(); 
-
-    // Disable the Peripheral Interrupts 
-    //INTERRUPT_PeripheralInterruptDisable(); 
 	IO_RA5_SetHigh();
 	
 	__delay_ms(50);
@@ -151,7 +136,6 @@ int main(void)
 				
 				break;
 		}
-		NOP();
 		
 		//Read back command from UART
 		command = read_command();
