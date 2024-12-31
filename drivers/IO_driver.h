d44d68b93d90b660e648272f6ed130a31ea80136
TychoBECH <s1u.tycho.kropf@gmail.com>
2024-12-31 23:03:24 +0100

    Added files and projekt structure for io driver

---
+++ drivers/IO_driver.h
@@ -0,0 +1,207 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   IO_driver.h
+ * Author: Tycho Kropf
+ * Comments:
+ * Revision history: 0.0.1
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef IO_DRIVER_H
+#define IO_DRIVER_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <stdint.h>
+
+/**
+    <p><b>Function prototype:</b></p>
+    void IO_Init(void);
+
+    <p><b>Summary:</b></p>
+    Initializes the IO pins for the microcontroller.
+
+    <p><b>Description:</b></p>
+    Configures all necessary pins for the application, setting directions, initial states,
+    and any special functionalities like pull-up or pull-down resistors if required.
+
+    <p><b>Precondition:</b></p>
+    None.
+
+    <p><b>Parameters:</b></p>
+    None.
+
+    <p><b>Returns:</b></p>
+    None.
+
+    <p><b>Example:</b></p>
+    <code>
+    IO_Init();
+    </code>
+
+    <p><b>Remarks:</b></p>
+    Ensure that any external devices connected to IO pins are in a safe state before calling this function.
+ */
+void IO_Init(void);
+
+/**
+    <p><b>Function prototype:</b></p>
+    void IO_Deinit(void);
+
+    <p><b>Summary:</b></p>
+    Deinitializes the IO pins, resetting them to their default state.
+
+    <p><b>Description:</b></p>
+    Resets all configured pins to their power-on default state, such as setting pins to inputs
+    and disabling any special functionalities.
+
+    <p><b>Precondition:</b></p>
+    IO_Init must have been called previously.
+
+    <p><b>Parameters:</b></p>
+    None.
+
+    <p><b>Returns:</b></p>
+    None.
+
+    <p><b>Example:</b></p>
+    <code>
+    IO_Deinit();
+    </code>
+
+    <p><b>Remarks:</b></p>
+    Use this function to safely shut down IO configurations before powering down the system or resetting.
+ */
+void IO_Deinit(void);
+
+/**
+    <p><b>Function prototype:</b></p>
+    void IO_SetPinDirection(uint8_t pin, bool direction);
+
+    <p><b>Summary:</b></p>
+    Sets the direction of a specific IO pin.
+
+    <p><b>Description:</b></p>
+    Configures the specified pin as an input or output, depending on the provided direction.
+
+    <p><b>Precondition:</b></p>
+    IO_Init must have been called previously.
+
+    <p><b>Parameters:</b></p>
+    <ul>
+        <li>uint8_t pin - The pin number to configure.</li>
+        <li>bool direction - The desired direction (true for output, false for input).</li>
+    </ul>
+
+    <p><b>Returns:</b></p>
+    None.
+
+    <p><b>Example:</b></p>
+    <code>
+    IO_SetPinDirection(5, true); // Set pin 5 as output
+    </code>
+
+    <p><b>Remarks:</b></p>
+    Ensure that the pin number is valid and within the range of available IO pins.
+ */
+void IO_SetPinDirection(uint8_t pin, bool direction);
+
+/**
+    <p><b>Function prototype:</b></p>
+    void IO_WritePin(uint8_t pin, bool value);
+
+    <p><b>Summary:</b></p>
+    Writes a digital value to a specific IO pin.
+
+    <p><b>Description:</b></p>
+    Sets the specified pin to the given digital value (high or low).
+
+    <p><b>Precondition:</b></p>
+    The pin must be configured as an output using IO_SetPinDirection.
+
+    <p><b>Parameters:</b></p>
+    <ul>
+        <li>uint8_t pin - The pin number to set.</li>
+        <li>bool value - The digital value to write (true for high, false for low).</li>
+    </ul>
+
+    <p><b>Returns:</b></p>
+    None.
+
+    <p><b>Example:</b></p>
+    <code>
+    IO_WritePin(5, true); // Set pin 5 to high
+    </code>
+
+    <p><b>Remarks:</b></p>
+    Writing to a pin configured as input may have no effect or cause undefined behavior.
+ */
+void IO_WritePin(uint8_t pin, bool value);
+
+/**
+    <p><b>Function prototype:</b></p>
+    bool IO_ReadPin(uint8_t pin);
+
+    <p><b>Summary:</b></p>
+    Reads the digital value of a specific IO pin.
+
+    <p><b>Description:</b></p>
+    Returns the current digital value (high or low) of the specified pin.
+
+    <p><b>Precondition:</b></p>
+    The pin must be configured as an input using IO_SetPinDirection.
+
+    <p><b>Parameters:</b></p>
+    <ul>
+        <li>uint8_t pin - The pin number to read.</li>
+    </ul>
+
+    <p><b>Returns:</b></p>
+    <ul>
+        <li>bool - The digital value of the pin (true for high, false for low).</li>
+    </ul>
+
+    <p><b>Example:</b></p>
+    <code>
+    bool pinState = IO_ReadPin(5); // Read the state of pin 5
+    </code>
+
+    <p><b>Remarks:</b></p>
+    Ensure that the pin number is valid and within the range of available IO pins.
+ */
+bool IO_ReadPin(uint8_t pin);
+
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif // IO_DRIVER_H
+
---
+++ drivers/io_driver.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   io_driver.c
+ * Author: Tycho Kropf
+ * Created on: 2024-12-31
+ *
+ * Description:
+ * This file contains the implementation of the IO driver for handling GPIO pins. 
+ * It defines initialization, deinitialization, and runtime control functions for
+ * configuring pin directions and reading/writing pin states. The design uses a 
+ * struct-based approach to encapsulate pin-specific properties and functions, 
+ * enabling flexibility and modularity.
+ *
+ * Revision History:
+ * Version 0.0.1: 
+ *   - Initial implementation with basic IO functionality.
+ *   - Includes support for pin initialization, direction setting, value writing, 
+ *     and value reading.
+ */
+
+
