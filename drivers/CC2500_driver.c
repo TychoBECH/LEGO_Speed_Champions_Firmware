1eed0bb0e70a62fdd1f3918e931bfc62b3cb0808
TychoBECH <s1u.tycho.kropf@gmail.com>
2025-02-26 13:19:24 +0100

    Implemented initialisation for CC2500

--- drivers/CC2500_driver.c
+++ drivers/CC2500_driver.c
@@ -11,8 +11,30 @@
  * Version 0.0.1: 
  *
  */
+#include "CC2500_driver.h"
 
 void CC2500_init(void){
-	
+	CC2500_WriteRegister(CC2500_IOCFG0,0x06);	//Set GDO0 to Asserts when sync word has been sent / received, and de-asserts at the end of the packet.
+	CC2500_WriteRegister(CC2500_PKTCTRL0,0x05);	//Variable packet length mode, CRC calculation in TX and RX enabled
+	CC2500_WriteRegister(CC2500_CHANNR,0x03);	//Set to Channel 3
+	CC2500_WriteRegister(CC2500_FSCTRL1,0x08);	//intermideat frequency of 203.125 kHz -> No idea what this does...
+	CC2500_WriteRegister(CC2500_FREQ2,0x5D);	//set carrier frequency to 2.425 GHz
+	CC2500_WriteRegister(CC2500_FREQ1,0x44);	//
+	CC2500_WriteRegister(CC2500_MDMCFG4,0x86);	//
+	CC2500_WriteRegister(CC2500_MDMCFG3,0x83);
+	CC2500_WriteRegister(CC2500_MDMCFG2,0x03);
+	CC2500_WriteRegister(CC2500_MDMCFG1,0x23);
+	CC2500_WriteRegister(CC2500_MDMCFG0,0x3B);
+	CC2500_WriteRegister(CC2500_DEVIATN,0x44);
+	CC2500_WriteRegister(CC2500_MCSM0,0x18);
+	CC2500_WriteRegister(CC2500_FOCCFG,0x16);
+	CC2500_WriteRegister(CC2500_FSCAL1,0x00);
+	CC2500_WriteRegister(CC2500_FSCAL0,0x11);
 }
 
+void CC2500_WriteRegister(uint8_t reg, uint8_t value){
+	MSSP1_SPI_Start();
+	MSSP1_SPI_WriteByte(reg & CC2500_SingleWrite);
+	MSSP1_SPI_WriteByte(value);
+	MSSP1_SPI_Stop();
+}
--- drivers/CC2500_driver.h
+++ drivers/CC2500_driver.h
@@ -33,8 +33,10 @@
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 
+#include "mssp_driver.h"
 // TODO Insert appropriate #include <>
 
+//<editor-fold desc="CC2500 State Definitions">
 #define STATE_IDLE 0b000
 #define STATE_RX 0b001
 #define STATE_TX 0b010
@@ -43,8 +45,64 @@
 #define STATE_SETTLING 0b101
 #define STATE_RXFIFO_OVERFLOW 0b110
 #define STATE_TXFIFO_UNDERFLOW 0b110
+//</editor-fold>
 
-// TODO Insert declarations
+//<editor-fold desc="CC2500 Register Definitions">
+#define CC2500_IOCFG2   0x00
+#define CC2500_IOCFG1   0x01
+#define CC2500_IOCFG0   0x02
+#define CC2500_FIFOTHR  0x03
+#define CC2500_SYNC1    0x04
+#define CC2500_SYNC0    0x05
+#define CC2500_PKTLEN   0x06
+#define CC2500_PKTCTRL1 0x07
+#define CC2500_PKTCTRL0 0x08
+#define CC2500_ADDR     0x09
+#define CC2500_CHANNR   0x0A
+#define CC2500_FSCTRL1  0x0B
+#define CC2500_FSCTRL0  0x0C
+#define CC2500_FREQ2    0x0D
+#define CC2500_FREQ1    0x0E
+#define CC2500_FREQ0    0x0F
+#define CC2500_MDMCFG4  0x10
+#define CC2500_MDMCFG3  0x11
+#define CC2500_MDMCFG2  0x12
+#define CC2500_MDMCFG1  0x13
+#define CC2500_MDMCFG0  0x14
+#define CC2500_DEVIATN  0x15
+#define CC2500_MCSM2    0x16
+#define CC2500_MCSM1    0x17
+#define CC2500_MCSM0    0x18
+#define CC2500_FOCCFG   0x19
+#define CC2500_BSCFG    0x1A
+#define CC2500_AGCCTRL2 0x1B
+#define CC2500_AGCCTRL1 0x1C
+#define CC2500_AGCCTRL0 0x1D
+#define CC2500_WOREVT1  0x1E
+#define CC2500_WOREVT0  0x1F
+#define CC2500_WORCTRL  0x20
+#define CC2250_FREND1   0x21
+#define CC2250_FREND0   0x22
+#define CC2500_FSCAL3   0x23
+#define CC2500_FSCAL2   0x24
+#define CC2500_FSCAL1   0x25
+#define CC2500_FSCAL0   0x26
+#define CC2500_RCCTRL1  0x27
+#define CC2500_RCCTRL0  0x28
+#define CC2500_FSTEST   0x29
+#define CC2500_PTEST    0x2A
+#define CC2500_AGCTEST  0x2B
+#define CC2500_TEST2    0x2C
+#define CC2500_TEST1    0x2D
+#define CC2500_TEST0    0x2E
+//</editor-fold>
+
+//<editor-fold desc="CC2500 R/W and burst flags">
+#define CC2500_SingleWrite  0x00
+#define CC2500_BurstWrite   0x80
+#define CC2500_SingleRead   0x40
+#define CC2500_BurstRead    0xC0
+//</editor-fold>
 
 // Comment a function and leverage automatic documentation with slash star star
 /**
@@ -68,8 +126,12 @@
     <p><b>Remarks:</b></p>
  */
 void CC2500_init(void);
-// TODO Insert declarations or function prototypes (right here) to leverage 
-// live documentation
+
+void CC2500_WriteRegister(uint8_t reg, uint8_t value);
+
+uint8_t CC2500_ReadRegister(uint8_t reg);
+
+uint8_t CC2500_TransferRegister(uint8_t reg, uint8_t value);
 
 #ifdef	__cplusplus
 extern "C" {
